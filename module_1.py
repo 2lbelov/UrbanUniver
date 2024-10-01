1fe1029315a53b420cafddf5c37e44cad50239b1
2lbelov <2lbelov78@gmail.com>
2024-10-01 14:52:55 +0300

    Add files via upload

---
+++ module_1.py
@@ -0,0 +1,7 @@
+course_name = 'Python'
+total_tasks = 12
+spent_hours = 1.5
+print('Курс: ' + course_name + ',', 'всего задач: ' + str(total_tasks) + ',', 'затрачено часов: ' + str(spent_hours) + ',','среднее время выполнения', spent_hours / total_tasks, 'часа.')
+print('Курс: ', course_name, ',', 'всего задач: ', total_tasks, ',', 'затрачено часов: ', spent_hours, ',','среднее время выполнения', spent_hours / total_tasks, 'часа.')
+
+#Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
